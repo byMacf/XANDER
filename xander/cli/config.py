10c972530182a5d5b6ac9fc378669e5dd41914cd
DM <dominicmacfarlane1@hotmail.com>
2022-04-24 00:39:40 +0100

    Fixing file paths to be more dynamic

--- xander/cli/config.py
+++ xander/cli/config.py
@@ -1,5 +1,6 @@
 import click
 import yaml
+import os
 
 from jinja2 import Environment, FileSystemLoader
 
@@ -15,6 +16,7 @@ def build(device, vendor, section):
 	Takes: 
 	name: Device to build configuration for
 	'''
+	ROOT_DIR = os.getcwd()
 	section_list = ['acls', 'interfaces', 'macsec', 'policies', 'routing', 'system']
 
 	if not vendor:
@@ -22,21 +24,21 @@ def build(device, vendor, section):
 	if not section:
 		section = 'all'
 
-	template_path = Environment(loader=FileSystemLoader('/Users/dom/Documents/Py/XANDER/xander/templates/' + vendor), trim_blocks=True, lstrip_blocks=True)
-	device_vars = yaml.load(open('/Users/dom/Documents/Py/XANDER/xander/vars/' + device + '.yaml'), Loader=yaml.SafeLoader)
+	template_path = Environment(loader=FileSystemLoader(ROOT_DIR + '/xander/templates/' + vendor), trim_blocks=True, lstrip_blocks=True)
+	device_vars = yaml.load(open(ROOT_DIR + '/xander/vars/' + device + '.yaml'), Loader=yaml.SafeLoader)
 
 	if section in section_list:
 		template = template_path.get_template(section + '.j2')
 		built_config = template.render(device_vars)
 
-		with open('/Users/dom/Documents/Py/XANDER/xander/configs/' + device + '_' + section + '.txt', 'w') as built_config_file:
+		with open(ROOT_DIR + '/xander/configs/' + device + '_' + section + '.txt', 'w') as built_config_file:
 			built_config_file.write(built_config)
 	else:
 		for section in section_list:
 			template = template_path.get_template(section + '.j2')
 			built_config = template.render(device_vars)
 
-			with open('/Users/dom/Documents/Py/XANDER/xander/configs/' + device + '_all.txt', 'a') as built_config_file:
+			with open(ROOT_DIR + '/xander/configs/' + device + '_all.txt', 'a') as built_config_file:
 				built_config_file.write(built_config)
 
 @click.group(short_help = 'Configuration commands')
